5aa858ba8e57c3896af9d2763578bd6dd7b0fda6
mxrkedz <pabelicojohnmark@gmail.com>
2024-08-23 22:01:07 +0800

    header

---
+++ frontend/src/app/components/header.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./css/header.module.css";
+import Link from "next/link";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.container} ${styles["container-md"]}`}>
+        <nav className={`${styles.nav} ${styles["nav-md"]}`}>
+          <Link href="#" className={styles["nav-link"]} prefetch={false}>
+            Events
+          </Link>
+          <Link href="#" className={styles["nav-link"]} prefetch={false}>
+            Discover
+          </Link>
+          <Link href="#" className={styles["nav-link"]} prefetch={false}>
+            About
+          </Link>
+        </nav>
+        <Link
+          href="#"
+          className={`${styles.link} ${styles["nav-md"]}`}
+          prefetch={false}
+        >
+          <span className={styles.brand}>Evently BLink</span>
+        </Link>
+        <div className={`${styles.nav} ${styles["nav-md"]}`}>
+          <Button size="small" variant="outlined" color="inherit">
+            Create Event
+          </Button>
+          <Avatar
+            sx={{ bgcolor: "white", width: 24, height: 24 }}
+            alt="Guest"
+            src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,12 +11,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
