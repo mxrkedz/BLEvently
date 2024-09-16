a65a9e077a55794fa507b7bc35db1487a242b081
mxrkedz <pabelicojohnmark@gmail.com>
2024-09-17 02:09:35 +0800

    error_handler

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
 
 config({
   path: "./data/config.env",
@@ -10,4 +11,6 @@ export const app = express();
 app.use(express.json());
 
 import user from "./routes/user.js";
-app.use("/api/v1/user",user);
+app.use("/api/v1/user", user);
+
+app.use(errorMiddleware);
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,21 +21,21 @@ export const login = asyncError(async (req, res, next) => {
   const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Incorrect Email or Password" });
+    return next(new Error("Incorrect Email or Password", 400));
   }
+
+  if (!password)
+    return next(new ErrorHandler("Please Enter The Password", 400));
 
   const isMatched = await user.comparePassword(password);
 
   if (!isMatched) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Invalid Credentials" });
+    return next(new Error("Incorrect Email or Password", 400));
   }
 
   res.status(200).json({
     success: true,
     message: `Welcome Back, ${user.name}!`,
   });
+  next(new Error());
 });
