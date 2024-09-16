3149e885fd25ff398f7185e95e95e34cf6aeaddd
mxrkedz <pabelicojohnmark@gmail.com>
2024-09-17 01:07:40 +0800

    setup_backend

---
+++ backend/models/event.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 100,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["Convention", "Expo", "Music"],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  organizer: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+//   images: [
+//     {
+//       public_id: {
+//         type: String,
+//         required: true,
+//       },
+//       url: {
+//         type: String,
+//         required: true,
+//       },
+//     },
+//   ],
+
+});
+
+module.exports = mongoose.model("Event", eventSchema);
